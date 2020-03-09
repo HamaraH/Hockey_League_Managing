c39ad281e6d07917e4fc82b5baee7ab659232b99
FLedeux <44844255+FLedeux@users.noreply.github.com>
2020-03-09 14:12:32 -0400

    Add files via upload

---
+++ HPP/CalendrierRencontre.hpp
@@ -0,0 +1,29 @@
+#ifndef CALENDRIERRENCONTRE_H
+#define CALENDRIERRENCONTRE_H
+
+#include "Rencontre.hpp"
+
+
+class CalendierRencontre{
+
+    private :
+      int annee;
+      std::vector<Rencontre> rencontres;
+
+    public :
+
+    CalendierRencontre();
+    CalendierRencontre(int annee);
+    CalendierRencontre(int annee, std::vector<Rencontre> rencontres);
+    ~CalendierRencontre();
+
+    int getAnnee();
+    void setAnnee(int annee);
+    std::vector<Rencontre> getRencontres();
+    void setRencontres(std::vector<Rencontre> rencontres);
+
+
+};
+
+
+#endif
